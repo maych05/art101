53fe6adb1c6490e1e42ffbe47b9d68011189b53f
May Cho <mcho37@ucsc.edu>
2025-04-21 16:07:05 -0700

    Lab 4 Changes
    JS psedocode game, HTML Lab 4 changes , added a lot of images

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: May Cho
+// Date: 4/18/24
 
 // Constants
 
@@ -8,8 +8,20 @@
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
-  // some code here
-  // return results;
+  // Generate 3x3 grid of shuffled symbols
+      // 3 pairs of matching symbols
+      // Symbols should not be right next to each other
+  //Display symbols for 4 seconds for player to memorize
+    // Cover symbols
+
+  //Players pick two covered symbols at a time
+   // Accept user input;
+
+   //Validate user input
+    //If symbols match, do not cover
+    //If symbols aren't a match, cover
+
+  //Repeat until all symbols are uncovered 
 }
 
 function main() {
